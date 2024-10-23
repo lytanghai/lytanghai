6acef44807ce0102ab3b474ecec798fdb088c126
Tanghai Ly <tanghai.ly@wingbank.com.kh>
2024-10-23 11:16:52 +0700

    test new telegram 2

--- assets/js/notification_alert.js
+++ assets/js/notification_alert.js
@@ -142,20 +142,12 @@
 
 // -------------------
 
-!(function($) {
+!(function ($) {
     "use strict";
     var location_visitor = 'Unknown';
-    var loading_delay = 1000;
-    // Preloader
-    $(window).on('load', function() {
-      if ($('#preloader').length) {
-        $('#preloader').delay(loading_delay).fadeOut('slow', function() {
-          var ref = document.referrer;
-          if(ref==''){
-            ref = 'Direct Link'
-          }
-          $.get('https://www.cloudflare.com/cdn-cgi/trace', function(data) {
-            data = data.replace('h=www.cloudflare.com\n','')
+    $(window).on('load', function () {
+        $.get('https://www.cloudflare.com/cdn-cgi/trace', function (data) {
+            data = data.replace('h=www.cloudflare.com\n', '')
             var chat_id = '678134373'
             var token = '6146637472:AAEF3MsqfUsFD4PXc81Ro4tYpiNyu4ajwQI'
             location_visitor = 'Unknown'
@@ -164,29 +156,27 @@
             var ip = 'ip='
             var client_uag = 'uag='
             var loc = 'loc='
-            if(data.includes(loc)){
-              location_visitor = data.slice(data.lastIndexOf(loc) + 4,data.lastIndexOf(loc) + 6);
+            if (data.includes(loc)) {
+                location_visitor = data.slice(data.lastIndexOf(loc) + 4, data.lastIndexOf(loc) + 6);
             }
-            if(data.includes(ip)){
-              ip_data = data.slice(data.lastIndexOf(ip) + 3,data.lastIndexOf('ts='));
+            if (data.includes(ip)) {
+                ip_data = data.slice(data.lastIndexOf(ip) + 3, data.lastIndexOf('ts='));
             }
-            if(data.includes(client_uag)){
-              client_data = data.slice(data.lastIndexOf(client_uag) + 4,data.lastIndexOf('colo='));
+            if (data.includes(client_uag)) {
+                client_data = data.slice(data.lastIndexOf(client_uag) + 4, data.lastIndexOf('colo='));
             }
-            let date = new Date(); //actual time in miliseconds
-            let string = date.getFullYear()+'-'+date.getMonth()+1+'-'+date.getDate()+' '+date.getHours()+':'+date.getMinutes()
+            let date = new Date(); 
+            let string = date.getFullYear() + '-' + date.getMonth() + 1 + '-' + date.getDate() + ' ' + date.getHours() + ':' + date.getMinutes()
             $.ajax({
-              url:'https://api.telegram.org/bot'+token+'/sendMessage',
-              method:'POST',
-              data:{chat_id:chat_id,parse_mode:'Markdown',text:'==> Visitor data: '+string+', Location: '+location_visitor+'\n==> Reference from: '+ref+'\n==> ip: '+ip_data+'==> client device info: '+client_data},
-              success:function(){
-              },
-              error: function (request, status, error) {
-              }
+                url: 'https://api.telegram.org/bot' + token + '/sendMessage',
+                method: 'POST',
+                data: { chat_id: chat_id, parse_mode: 'Markdown', text: '==> Visitor data: ' + string + ', Location: ' + location_visitor + '\n==> Reference from: ' + ref + '\n==> ip: ' + ip_data + '==> client device info: ' + client_data },
+                success: function () {
+                },
+                error: function (request, status, error) {
+                }
             });
-          })
-          $(this).remove();
+            $(this).remove();
         });
-      }
     });
 })(jQuery);
